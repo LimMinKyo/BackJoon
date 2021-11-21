d312a0255c32476722ac0e58e35ac7c3f6b2511c
LimMinKyo <story1400@naver.com>
2021-11-21 20:45:12 +0900

    Solve 10998

---
+++ 01. 입출력과 사칙연산/07. AxB/10998.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+// const filePath = "./01. 입출력과 사칙연산/07. AxB/input.txt";
+// const input = fs.readFileSync(filePath).toString().split(' ').map(val => +val);
+const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(val => +val);
+const A = input[0];
+const B = input[1];
+console.log(A*B);
