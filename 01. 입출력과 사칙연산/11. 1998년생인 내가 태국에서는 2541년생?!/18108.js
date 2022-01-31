388171f12f00b42f634975de8bf98278b70fd9d6
LimMinKyo <story1400@naver.com>
2022-01-31 16:29:39 +0900

    Solve 18108

---
+++ 01. 입출력과 사칙연산/11. 1998년생인 내가 태국에서는 2541년생?!/18108.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+// const filePath = './01. 입출력과 사칙연산/11. 1998년생인 내가 태국에서는 2541년생?!/input.txt';
+// const input = parseInt(fs.readFileSync(filePath).toString());
+const input = parseInt(fs.readFileSync('/dev/stdin').toString());
+
+console.log(input - 543);
