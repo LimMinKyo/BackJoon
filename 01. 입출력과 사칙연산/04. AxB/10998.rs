5b990d57ceefcf4ebf56c86e946e9a479f2c2e1f
LinMinKyo <story1400@naver.com>
2024-05-27 14:18:14 +0900

    Solve 10998

---
+++ 01. 입출력과 사칙연산/04. AxB/10998.rs
@@ -0,0 +1,14 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input_numbers = String::new();
+
+    io::stdin().read_to_string(&mut input_numbers).unwrap();
+
+    let numbers: Vec<i32> = input_numbers
+        .split_whitespace()
+        .map(|x| x.parse().expect("num_err"))
+        .collect();
+
+    println!("{}", numbers[0] * numbers[1]);
+}
